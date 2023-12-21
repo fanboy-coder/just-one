42936638f6f7d167156a62c7e218e1dc822ba6d3
Joel Monteiro <joel.monteiro.ferreira@gmail.com>
2023-12-21 18:29:35 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React>
     <App />
-  </React.StrictMode>,
+  </React>,
 )
